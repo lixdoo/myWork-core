fd6c76e55eddc6bb1742d6e25af4ad1abb02e85f
lixdoo <437526826@qq.com>
2017-02-08 17:20:47 +0800

    init

---
+++ myWork-core/src/main/java/org/myWork/core/model/AbstractObject.java
@@ -0,0 +1,24 @@
+package org.myWork.core.model;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public abstract class AbstractObject implements Serializable, Cloneable {
+	public static final long serialVersionUID = 1L;
+	
+	public String toString(){
+		return ToStringBuilder.reflectionToString(this);
+	}
+	
+	public Object clone(){
+		Object obj = null;
+		try {
+			obj = super.clone();
+		}catch (CloneNotSupportedException e){
+			e.printStackTrace();
+		}
+		return obj;
+	}
+
+}
